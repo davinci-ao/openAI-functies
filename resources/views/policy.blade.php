61a9548de298af60d3d6d650783278790d38bfca
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-25 11:14:22 +0200

    Add privacy policy page

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,13 +1,15 @@
-<x-guest-layout>
-    <div class="pt-4 bg-gray-100">
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-            <div>
-                <x-authentication-card-logo />
-            </div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Privacy Policy') }}
+        </h2>
+    </x-slot>
 
-            <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
-                {!! $policy !!}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-policy />
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-app-layout>
