fbd32df4357117d86f91881d2d997855ed1a0af7
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-25 11:13:04 +0200

    Add basic privacy policy

---
+++ resources/views/components/policy.blade.php
@@ -0,0 +1,105 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+{{--    <x-application-logo class="block h-12 w-auto" />--}}
+
+    <h1 class="mt-8 text-2xl font-medium text-gray-900">
+        Privacy Policy
+    </h1>
+
+    <p class="mt-6 text-gray-500 leading-relaxed">
+        Onze privacy policy vertelt u meer over de data en de rechten die u hebt als gebruiker van onze service. U kunt hieronder vinden welke data wij verzamelen, en welke data mogelijk verzameld wordt door OpenAI.
+        <b class="text-yellow-500">Deel nooit uw persoonlijke informatie met OpenAI via onze aangeboden services. Uw privacy en veiligheid is dan niet gegarandeerd.</b>
+    </p>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 gap-6 lg:gap-8 p-6 lg:p-8">
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+            </svg>
+            <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                Data die wij verzamelen
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Om optimaal gebruik van de website te garanderen, moeten wij de volgende gebruikersdata verzamelen, opslaan en gebruiken:<br>
+            <b>naam, mail, tijdstip van mail verificatie, wachtwoord en profielfoto. Deze worden gebruikt voor inloggen, registratie en gebruiker-specifieke doeleindes</b>.
+        </p>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Om optimaal gebruik en veiligheid voor onze gebruikers te garanderen, moeten wij de volgende sessiedata verzamelen, opslaan en gebruiken:<br>
+            <b>IP adres, user agent, payload, laatste activiteit. Het IP, samen met de user agent per sessie kunt u vinden op uw profiel pagina.</b>
+        </p>
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-gray-400" viewBox="0 0 20 20" fill="none">
+                <path fill-rule="evenodd" d="M6.625 2.655A9 9 0 0119 11a1 1 0 11-2 0 7 7 0 00-9.625-6.492 1 1 0 11-.75-1.853zM4.662 4.959A1 1 0 014.75 6.37 6.97 6.97 0 003 11a1 1 0 11-2 0 8.97 8.97 0 012.25-5.953 1 1 0 011.412-.088z" clip-rule="evenodd"></path>
+                <path fill-rule="evenodd" d="M5 11a5 5 0 1110 0 1 1 0 11-2 0 3 3 0 10-6 0c0 1.677-.345 3.276-.968 4.729a1 1 0 11-1.838-.789A9.964 9.964 0 005 11zm8.921 2.012a1 1 0 01.831 1.145 19.86 19.86 0 01-.545 2.436 1 1 0 11-1.92-.558c.207-.713.371-1.445.49-2.192a1 1 0 011.144-.83z" clip-rule="evenodd"></path>
+                <path fill-rule="evenodd" d="M10 10a1 1 0 011 1c0 2.236-.46 4.368-1.29 6.304a1 1 0 01-1.838-.789A13.952 13.952 0 009 11a1 1 0 011-1z" clip-rule="evenodd"></path>
+            </svg>
+            <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                OpenAI - ChatGPT privacy policy
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            De content die je doorvoert aan ChatGPT via deze website kan mogelijke worden opgeslagen door OpenAI voor training- en research doeleindes.<br>
+            Wij hebben hier geen controle over. Wees voorzichtig met de data die je het toestopt. Geef nooit persoonlijke informatie of privé informatie door.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="https://openai.com/policies/privacy-policy" class="inline-flex items-center font-semibold text-indigo-700" target="_blank">
+                OpenAI Privacy Policy
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </p>
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-gray-400" viewBox="0 0 20 20" fill="none">
+                <path fill-rule="evenodd" d="M13.477 14.89A6 6 0 015.11 6.524l8.367 8.368zm1.414-1.414L6.524 5.11a6 6 0 018.367 8.367zM18 10a8 8 0 11-16 0 8 8 0 0116 0z" clip-rule="evenodd"></path>
+            </svg>
+            <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                Kinderen onder 16
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Wij zijn niet bekend met de leeftijd van onze gebruikers, en verzamelen dan ook niet intentioneel data van kinderen onder de 16.<br>
+            Je hebt alleen het recht om in te stemmen met deze Privacy Policy indien je 16 jaar of ouder bent, of na 13 jaar toestemming hebt van uw ouder/voogd.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="https://gdpr-info.eu/art-8-gdpr/" class="inline-flex items-center font-semibold text-indigo-700" target="_blank">
+                Artikel 8 - GDPR
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </p>
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 stroke-gray-400" viewBox="0 0 20 20" fill="none">
+                <path fill-rule="evenodd" d="M10 1.944A11.954 11.954 0 012.166 5C2.056 5.649 2 6.319 2 7c0 5.225 3.34 9.67 8 11.317C14.66 16.67 18 12.225 18 7c0-.682-.057-1.35-.166-2.001A11.954 11.954 0 0110 1.944zM11 14a1 1 0 11-2 0 1 1 0 012 0zm0-7a1 1 0 10-2 0v3a1 1 0 102 0V7z" clip-rule="evenodd"></path>
+            </svg>
+            <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                Vragen en problemen
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Voor vragen en problemen, vragen we u om ons een mail te sturen op <a href="mailto:99069867@mydavinci.nl">99069867@mydavinci.nl</a> en/of <a href="mailto:jslemmer@mydavinci.nl">jslemmer@mydavinci.nl</a>.
+            Mocht u een probleem hebben wat niet gerelateerd is aan onze Privacy Policy, vragen we u om een <a href="https://github.com/davinci-ao/openAI-functions/issues/new">issue te maken</a> op de repository.
+        </p>
+    </div>
+</div>
