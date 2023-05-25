4a0a6e59194f0f32796f9c1e1831142c42d4bb9e
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-25 11:25:55 +0200

    Add privacy navlink

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link href="{{ route('privacy') }}" :active="request()->routeIs('privacy')">
+                        {{ __('Privacy') }}
+                    </x-nav-link>
                 </div>
             </div>
 
