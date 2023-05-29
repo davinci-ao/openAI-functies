b85c926a018bf25097ec08e4815c12487fd7a3ef
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-29 18:59:49 +0200

    add language field & make fields larger

--- resources/views/function-validator.blade.php
+++ resources/views/function-validator.blade.php
@@ -8,9 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <form method="POST" action="{{ route('chatresponse') }}">
-                @csrf
-                <label for="userQuery">Jouw Functie</label>
-                <input type="text" name="userQuery" id="userQuery" placeholder="Your function here">
+                    <div class="w-4/5 m-5">
+                    @csrf
+                    <label class="inline-block w-2/12 text-center font-bold m-5 text-2xl" for="language">Taal</label>
+                    <textarea rows="2" cols="50" type="text" name="language" id="language" placeholder="php.. javascript.. python..."></textarea>
+                    <br>
+                    <label class="inline-block w-2/12 text-center font-bold m-5 text-2xl" for="userQuery">Jouw Functie</label>
+                    <textarea rows="4" cols="50" type="text" name="userQuery" id="userQuery" placeholder="Your function here"></textarea>
+                </div>
+                <button type="submit" class="uppercase bg-black text-gray-200 text-lg w-1/5 m-10 font-extrabold py-4 px-8 rounded-3xl hover:bg-sky-700">Submit</button>
             </form>
         </div>
 
