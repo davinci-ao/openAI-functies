c66e89882538fccaa321ab718443b58cd1cef62f
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-29 18:59:12 +0200

    add language

--- resources/views/chatresponse.blade.php
+++ resources/views/chatresponse.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h2 class="mt-6 text-xl font-semibold text-gray-900">Your code</h2>
+                <h2 class="mt-6 text-xl font-semibold text-gray-900">Your {{$_POST['language']}} code</h2>
                 <code>{{ $_POST['userQuery'] }}</code>
             </div>
 
