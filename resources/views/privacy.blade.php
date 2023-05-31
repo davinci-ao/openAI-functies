a1003f69a6488ee79fdeed4e1027e14cbf64cb5e
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-31 10:02:01 +0200

    show navbar depending on guest or auth

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <div class="flex inline-block d-inline-block text-center justify-center">
+        @auth
         <a href="/dashboard">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center p-3">Home</h2>
         </a>
@@ -7,6 +8,13 @@
         <a href="/validator">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center p-3">Validator</h2>
         </a>
+        @endauth
+
+        @guest
+        <a href="/">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight text-center p-3">Home</h2>
+        </a>
+        @endguest
     </div>
 
     <div class="py-12">
